2f33da7db285b843c96a90f62cb6e0d69bce9b35
dansResearch <dij502@york.ac.uk>
2020-07-21 09:58:02 +0100

    Initial
    Initial Commit

--- Assets/Scripts/Video Playing/RotateSkyBox.cs
+++ Assets/Scripts/Video Playing/RotateSkyBox.cs
@@ -4,30 +4,21 @@ using UnityEngine;
 
 public class RotateSkyBox : MonoBehaviour
 {
+    /// <summary>
+    /// this class will be used to rotate the 360 photo to match the angles of the audio rednering engine.
+    /// </summary>
   
     public float curRot = 0;
-
-
 
     private void Start()
     {
         RotateSky();
     }
 
-
-
     public void RotateSky()
-    {
-        
+    {        
         curRot %= 360;
         RenderSettings.skybox.SetFloat("_Rotation", curRot);
     }
 
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
